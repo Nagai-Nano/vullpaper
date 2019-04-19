c6c3c6d87289535c3c41a6060bbae1a2323ed70f
Nano <hikarishido1997@gmail.com>
2019-04-19 14:04:52 +0700

    set up express server

---
+++ server.js
@@ -0,0 +1,35 @@
+const isDev = process.env.NODE_ENV !== 'production';
+
+if (isDev) {
+  require('dotenv').config();
+}
+
+const { join } = require('path');
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+
+const app = express();
+const port = process.env.PORT || 1337;
+const dist = join(__dirname, 'dist');
+
+app.use(compression());
+app.use(helmet());
+app.use(express.static(dist));
+
+if (isDev) {
+  app.use(require('morgan')('dev'));
+  app.use(require('cors')());
+}
+
+app.get('/', (req, res) => res.send('hi'));
+
+if (!isDev) {
+  app.get('*', (req, res) => {
+    res.sendFile(join(dist, 'index.html'));
+  });
+}
+
+app.listen(port, () => {
+  console.log('Express is up on port', port);
+});
