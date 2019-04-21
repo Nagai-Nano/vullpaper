1f72a6b5f4291e7e20fc290be0fc02e303faa60a
Nano <hikarishido1997@gmail.com>
2019-04-21 14:11:15 +0700

    complete images view

---
+++ client/src/components/images/Images.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container>
+    <InfiniteScroll :loading="loading" :has-more="hasMore" @loadmore="loadmore">
+      <ImageGrid v-if="images.length" :images="images" />
+    </InfiniteScroll>
+    <Spinner v-if="loading" style="min-height: 10rem" size="medium" />
+  </v-container>
+</template>
+
+<script>
+import InfiniteScroll from '../images/InfiniteScroll';
+import ImageGrid from '../common/ImageGrid';
+import Spinner from '../common/Spinner';
+import request from '@/lib/request';
+
+export default {
+  props: {
+    q: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      loading: false,
+      page: 1,
+      hasMore: false,
+      images: []
+    };
+  },
+  components: {
+    ImageGrid,
+    Spinner,
+    InfiniteScroll
+  },
+  methods: {
+    async loadmore() {
+      this.loading = true;
+      this.page++;
+
+      const response = await request(`/posts/${this.q}?page=${this.page}`);
+      this.images = [...this.images, ...response.posts];
+      this.hasMore = response.hasMore;
+
+      this.loading = false;
+    }
+  },
+  async created() {
+    this.loading = true;
+
+    const response = await request(`/posts/${this.q}`);
+    this.images = response.posts;
+    this.hasMore = response.hasMore;
+
+    this.loading = false;
+  }
+};
+</script>
--- client/src/views/Images.vue
+++ client/src/views/Images.vue
@@ -2,25 +2,13 @@
   <v-container fluid ma-0 pa-0>
     <Intro :q="q" />
 
-    <v-container>
-      <InfiniteScroll
-        :loading="loading"
-        :has-more="hasMore"
-        @loadmore="loadmore"
-      >
-        <ImageGrid v-if="images.length" :images="images" />
-      </InfiniteScroll>
-      <Spinner v-if="loading" style="min-height: 10rem" size="medium" />
-    </v-container>
+    <Images :q="q" />
   </v-container>
 </template>
 
 <script>
 import Intro from '@/components/images/Intro';
-import ImageGrid from '@/components/common/ImageGrid';
-import Spinner from '@/components/common/Spinner';
-import InfiniteScroll from '@/components/images/InfiniteScroll';
-import request from '@/lib/request';
+import Images from '@/components/images/Images';
 
 export default {
   props: {
@@ -29,40 +17,9 @@ export default {
       required: true
     }
   },
-  data() {
-    return {
-      loading: false,
-      page: 1,
-      hasMore: false,
-      images: []
-    };
-  },
   components: {
     Intro,
-    ImageGrid,
-    Spinner,
-    InfiniteScroll
-  },
-  methods: {
-    async loadmore() {
-      this.loading = true;
-      this.page++;
-
-      const response = await request(`/posts/${this.q}?page=${this.page}`);
-      this.images = [...this.images, ...response.posts];
-      this.hasMore = response.hasMore;
-
-      this.loading = false;
-    }
-  },
-  async created() {
-    this.loading = true;
-
-    const response = await request(`/posts/${this.q}`);
-    this.images = response.posts;
-    this.hasMore = response.hasMore;
-
-    this.loading = false;
+    Images
   }
 };
 </script>
